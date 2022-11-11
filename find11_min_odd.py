42535e9dace9a977b0ddce6f6bc31a4faf99695a
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-11 11:36:47 +0500

    done

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -5,5 +5,11 @@ def find_min_odd(data):
         data: list of numbers
     returns: minimum odd number in the list
     """
-    return 0
+    m = 1000
+    for i in data:
+        if i%2==1:
+            if m > i:
+                m = i
 
+    return m if m!=1000 else -1
+print(find_min_odd([1, 8, 2, 8, 5]))
