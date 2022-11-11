c482b7fb487ebc7096de6c778cb0879f8c9bf428
RustambekSafarov <rustambeksafarovv@gmail.com>
2022-11-11 11:27:54 +0500

    done

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,11 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    m = -1000
+    for i in data:
+        if i%2==1:
+            if m < i:
+                m = i
+    
+    return m if m!=-1000 else -1
+print(find_max_odd([2, 8, 2, 8, -5]))
